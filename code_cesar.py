588e76d7a80f0a19e9a1abfa5e0afc9ee6daf095
Uramix <158606954+Uramix2@users.noreply.github.com>
2025-02-07 22:16:11 +0100

    Update code_cesar.py

--- code_cesar.py
+++ code_cesar.py
@@ -1,10 +1,11 @@
-lettre = {1: 'a',2: 'b',3: 'c',4: 'd', 5: 'e', 6: 'f', 7: 'g', 8: 'h', 9: 'i', 10: 'j',
-          11: 'k',12: 'l', 13: 'm', 14: 'n',15: 'o',
-          16: 'p',17: 'q',18: 'r',19: 's',20: 't',
-          21: 'u',22: 'v',23: 'w',24: 'x',25: 'y',26: 'z'} 
+# Description: Ce programme permet de crypter et décrypter un mot en utilisant le chiffrement de César.
+# Date de création: 04/02/2025 
+# Date de modification: 06/02/2025
+# Auteur: Uramix 
 
-def code_cesar(mot,espace):
+# --- Fonctions --- #
 
+def code_cesar(mot, espace):
     """
     Chiffre un mot en utilisant le chiffrement de César avec un décalage spécifié.
 
@@ -16,25 +17,31 @@ def code_cesar(mot,espace):
     str : Le mot crypté.
     """
     
+    espace = int(espace)  
     mot_cryp = "" 
-    espace = int(espace) 
-    
+
     for let in mot: 
-       for key , val in lettre.items():
-            if val == let:
-                  numero = key
-                  deca = (numero + espace ) % 26 
-                  new_lettre = lettre[deca]
-                  mot_cryp += new_lettre 
+        if "a" <= let <= "z":  
+            ascii_let = ord(let) + espace 
+            if ascii_let > ord('z'):  
+                ascii_let -= 26  
+            
+            mot_cryp += chr(ascii_let)
+        else:
+            mot_cryp += let  
+    
     return mot_cryp
 
 
 # --- Test --- # 
+
 print(code_cesar(input("Entrez le mot à crypter : "), input("Entrez un nombre : ")))
+
 # --- Test --- #
 
 
-def decrypt_avec_cle(mot,espace):
+
+def decrypt_avec_cle(mot, espace):
     """
     Déchiffre un mot en utilisant le chiffrement de César avec un décalage spécifié.
 
@@ -46,21 +53,23 @@ def decrypt_avec_cle(mot,espace):
     str : Le mot décrypté.
     """
     mot_decryp = "" 
-    espace = int(espace) 
+    espace = int(espace)  
     
     for let in mot: 
-       for key , val in lettre.items():
-            if val == let:
-                  numero = key
-                  deca = (numero - espace ) % 26 
-                  new_lettre = lettre[deca]
-                  mot_decryp += new_lettre 
+        if "a" <= let <= "z":  
+            ascii_let = ord(let) - espace
+            if ascii_let < ord('a'):  
+                ascii_let += 26  
+            
+            mot_decryp += chr(ascii_let)
+        else:
+            mot_decryp += let  
+    
     return mot_decryp
-
 
 # --- Test --- #
 
-print(decrypt_avec_cle(input("Entrez le mot à décrypter : "), input("Entrez une clé de chiffrement : ")))
+print(decrypt_avec_cle(input("Entrez le mot à décrypter : "), input("Entrez une cle de chiffrement : ")))
 
 # --- Test --- #
 
@@ -72,30 +81,36 @@ def decrypt_sans_cle(mot):
 
     Paramètres :
     mot (str) : Le mot à décrypter.
-    
-    Retourne :
-    str : Les 26 possibilités du mot décrypté.
+
+    Affiche :
+    Les 26 possibilités du mot décrypté.
     """
+    for deca in range(1, 27):
+        mot_decryp = ""
 
-    lettre = {1: 'a',2: 'b',3: 'c',4: 'd', 5: 'e', 6: 'f', 7: 'g', 8: 'h', 9: 'i', 10: 'j',
-          11: 'k',12: 'l', 13: 'm', 14: 'n',15: 'o',
-          16: 'p',17: 'q',18: 'r',19: 's',20: 't',
-          21: 'u',22: 'v',23: 'w',24: 'x',25: 'y',26: 'z'}
-    for deca in range(1,27):
-        mot_decryp = "" # Réinitialisation pour chaque test
         for let in mot:
-            if let in lettre.values():
+            if "a" <= let <= "z":  
                 ascii_let = ord(let) - deca
+                if ascii_let < ord('a'):
+                    ascii_let += 26  
                 mot_decryp += chr(ascii_let)
-                        
             else:
-                mot_decryp += let
-        print(f"[Décalage {deca:2d}] → {mot_decryp}") 
-                
-
+                mot_decryp += let  
+        print(f"Décalage {deca} → {mot_decryp}")
+    return "THE END"
 
 # --- Test --- #
 
 print(decrypt_sans_cle(input("Entrez le mot à décrypter : ")))
 
 # --- Test --- #
+
+         
+
+
+
+
+
+
+
+
